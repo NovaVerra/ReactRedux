4302caf7e246502261252d62d421cb6ca6390aa9
lununglee <leo.lunung.lee@gmail.com>
2019-10-31 14:05:48 -0700

    R17 - Handling Asynchronous Code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+// REACT PACKAGES
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+// REDUX PACKAGES
+import { createStore, combineReducers, compose, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import counterReducer from './store/reducers/counter'
 import resultsReducer from './store/reducers/results'
@@ -27,7 +30,7 @@ const logger = (store) => {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
